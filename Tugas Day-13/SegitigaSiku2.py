f0f91c1ed1c06763aba95c40585ccad9f9706457
Bobogeng <suirito@gmail.com>
2022-01-02 20:38:28 +0700

    Fixed

--- Tugas Day-13/SegitigaSiku2.py
+++ Tugas Day-13/SegitigaSiku2.py
@@ -6,10 +6,12 @@ class SegitigaSiku2:
         print("="*10, "Segitiga Siku-siku", 10*"=")
         self.a = int(input("Masukkan Alas Segitiga Siku-siku: "))
         self.t = int(input("Masukkan Tinggi Segitiga Siku-siku: "))
+        self.m = math.sqrt(math.pow(self.t, 2) + math.pow(self.a, 2))
 
     def keliling(self):
-        print("Keliling Segitiga Siku-siku:",
-              "%.1f" % (math.sqrt(math.pow(self.t, 2) + math.pow(self.a, 2))))
+
+        print("Keliling Segitiga Siku-siku:", "%.2f" %
+              (self.a * self.t * self.m))
 
     def luas(self):
         print("Luas Segitiga Siku-siku:", 0.5 * self.a * self.t, "\n")
